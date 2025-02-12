e6290e967b6e513df06a493c417a5679bb04d70f
liubaiqian <162894020+liubaiqian@users.noreply.github.com>
2025-02-12 11:42:50 +0800

    2025.2.12

---
+++ EasyJS.js
@@ -0,0 +1,13 @@
+var alt = alert;
+function randomMath(x){
+    return Math.ceil(Math.random()* x);
+}
+function docuGetById(id){
+    return document.getElementById(id);
+}
+function pmt(pmt){
+    return prompt(pmt);
+}
+function herfTo(herfTo){
+    return window.location.href= herfTo;
+}
